465c927f2a1f765f6bd1b182ff53c32a5efe85f0
amal <amalpaul0492@gmail.com>
2018-12-20 04:42:08 +0530

    added maps, loaded markers, got current location

--- app/src/main/java/com/amp/news/Fragments/WeatherFragment.java
+++ app/src/main/java/com/amp/news/Fragments/WeatherFragment.java
@@ -1,20 +1,38 @@
 package com.amp.news.Fragments;
 
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.amp.news.R;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class WeatherFragment extends Fragment {
+public class WeatherFragment extends Fragment implements OnMapReadyCallback {
 
+    private static final int REQUEST_LOCATION = 123;
+    private GoogleMap mMap;
+    private FusedLocationProviderClient mFusedLocationClient;
 
     public WeatherFragment() {
         // Required empty public constructor
@@ -30,7 +48,64 @@ public class WeatherFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_weather, container, false);
 
+        SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(getActivity());
         return view;
     }
+
+    private void checkPermission() {
+        if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(
+                    new String[]{android.Manifest.permission.ACCESS_COARSE_LOCATION,
+                            android.Manifest.permission.ACCESS_FINE_LOCATION},
+                    REQUEST_LOCATION);
+
+        } else {
+            // already permission granted
+            getLastKnownLocation();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == REQUEST_LOCATION) {
+            if (grantResults.length > 0
+                    && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                getLastKnownLocation();
+            }
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    private void getLastKnownLocation() {
+        mFusedLocationClient.getLastLocation().addOnSuccessListener(getActivity(), new OnSuccessListener<Location>() {
+            @Override
+            public void onSuccess(Location location) {
+                mMap.addMarker(new MarkerOptions().position(new LatLng(location.getLatitude(), location.getLongitude())).title("Marker in current location"));
+            }
+        });
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        checkPermission();
+        final LatLng INDIA = new LatLng(21.7679, 78.8718);
+        final LatLng DELHI = new LatLng(28.644800, 77.216721);
+        final LatLng KOLKATA = new LatLng(22.572645, 88.363892);
+        final LatLng MUMBAI = new LatLng(19.0760, 72.8777);
+        final LatLng CHENNAI = new LatLng(13.0827, 80.2707);
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(INDIA, 4.5f));
+        mMap.addMarker(new MarkerOptions().position(DELHI).title("Marker in delhi"));
+        mMap.addMarker(new MarkerOptions().position(KOLKATA).title("Marker in kolkata"));
+        mMap.addMarker(new MarkerOptions().position(MUMBAI).title("Marker in mumbai"));
+        mMap.addMarker(new MarkerOptions().position(CHENNAI).title("Marker in chennai"));
+
+    }
+
 
 }
